245840b3ec3a8439251de777bbb7a28c3f4e6760
aeonincode <khashayar.sasannia@hotmail.com>
2021-08-25 07:11:00 -0500

    start project

--- mac_changer.py
+++ mac_changer.py
@@ -2,4 +2,6 @@
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig eth1 down", shell=True)
+subprocess.call("ifconfig eth1 hw ether 00:11:22:33:44:66", shell=True)
+subprocess.call("ifconfig eth1 up", shell=True)
